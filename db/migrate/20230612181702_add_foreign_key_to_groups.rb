0775fb2c0a673fc94b79cbd0339ecb84a9fd0002
LeslieAine <aineleslie@gmail.com>
2023-06-14 10:35:46 +0300

    Modify db and update schema

--- db/migrate/20230612181702_add_foreign_key_to_groups.rb
+++ db/migrate/20230612181702_add_foreign_key_to_groups.rb
@@ -1,5 +1,5 @@
 class AddForeignKeyToGroups < ActiveRecord::Migration[7.0]
   def change
-    add_reference :groups, :users
+    add_foreign_key :groups, :users, column: :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_183844) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_190302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_183844) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "users_id"
-    t.index ["users_id"], name: "index_groups_on_users_id"
+    t.bigint "user_id"
   end
 
   create_table "groups_operations", id: false, force: :cascade do |t|
